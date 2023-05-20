b25d531c548add6f990c9278aa01748fbd9d052c
Lokikoki <125855689+Lokikoki@users.noreply.github.com>
2023-05-20 06:18:56 +0300

    new site :)

---
+++ script.js
@@ -0,0 +1,28 @@
+var indexValue = 1;
+  showImg(indexValue);
+  function btm_slide(e){showImg(indexValue = e);}
+  function side_slide(e){showImg(indexValue += e);}
+  function showImg(e){
+  var i;
+  const img = document.querySelectorAll('img');
+  const slider = document.querySelectorAll('.btm-slides span');
+  if(e > img.length){indexValue = 1}
+  if(e < 1){indexValue = img.length}
+  for(i = 0; i < img.length; i++){
+  img[i].style.display = "none";
+  }
+  for(i = 0; i < slider.length; i++){
+  slider[i].style.background = "rgba(255,255,255,0.1)";
+  }
+  img[indexValue-1].style.display = "block";
+  slider[indexValue-1].style.background = "white";
+    
+  $(function() {
+    var interval = setInterval(function(){
+     $(".pokazatel-snimka.right").click();
+    }, 5000); //задержка в миллисекундах
+    $(".btm-slides span, .pokazatel-snimka.left").click(function(){
+     clearInterval(interval); //остановка
+    });
+   });
+  }
